aafa78900ce809c716f74d1c6151e95dcfdff300
Jason Washburn <35488541+jasonwashburn@users.noreply.github.com>
2022-07-10 11:56:00 +0000

    adds get routes for product reviews

--- app/server/routes/product_review.py
+++ app/server/routes/product_review.py
@@ -12,3 +12,15 @@ router = APIRouter()
 async def add_product_review(review: ProductReview) -> dict:
     await review.create()
     return {"message": "Review added successfully"}
+
+
+@router.get("/{id}", response_description="Review record retrieved")
+async def get_review_record(id: PydanticObjectId) -> ProductReview:
+    review = await ProductReview.get(id)
+    return review
+
+
+@router.get("/", response_description="Review records retrieved")
+async def get_reviews() -> List[ProductReview]:
+    reviews = await ProductReview.find_all().to_list()
+    return reviews
